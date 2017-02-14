21b1744da722692ac1475bad914d48fedce31302
Matt Hill <2037856H@student.gla.ac.uk>
2017-02-14 10:57:39 +0000

    chapter 4

--- tangoFINAL/urls.py
+++ tangoFINAL/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 	url(r'^$', views.about, name='about'),
-	url(r'^rango/', include('rango.urls')),
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
