03912a200213ec464ea9189de56fa12042ed3914
Matt Hill <2037856H@student.gla.ac.uk>
2017-02-14 15:25:07 +0000

    new population script

--- rango/models.py
+++ rango/models.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+   name = models.CharField(max_length=128, unique=True)
+   views = models.IntegerField(default=0)
+   likes = models.IntegerField(default=0)
 
-
-    def __str__(self):  # For Python 2, use __unicode__ too
+   def __str__(self):  # For Python 2, use __unicode__ too
         return self.name
 
 	
