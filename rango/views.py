c74081973388b1e697797be054dd8282b2c031d9
Matt Hill <2037856H@student.gla.ac.uk>
2017-02-14 14:53:57 +0000

    fixed error

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-<<<<<<< HEAD
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
