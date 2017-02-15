1492f85d1f07b047452fb636cbed337d37ccf61f
Matt Hill <2037856H@student.gla.ac.uk>
2017-02-15 16:44:05 +0000

    chapter 10 complete

--- rango/views.py
+++ rango/views.py
@@ -11,21 +11,27 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from datetime import datetime
 
-def visitor_cookie_handler(request,response):
-	visits = int(request.COOKIES.get('visit','1'))
-	
-	last_visit_cookie = request.COOKIES.get('last_visit',str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
 	last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
 	
 	if (datetime.now() - last_visit_time).days >0:
 		visits = visits + 1
-		response.set_cookie('last_visit',str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
 		visits = 1
-		response.set_cookie('last_vist', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 	
-	response.set_cookie('visits', visits)
-
+	request.session['visits'] = visits
+	
 def index(request):
    context = RequestContext(request)
    category_list = Category.objects.order_by('-likes')[:5]
@@ -33,12 +39,12 @@ def index(request):
    page_list = Page.objects.order_by('-views')[:5]
    context_dict ['pages'] = page_list
    
-   visits = request.session.get('visits',0)
-   context_dict ['visits'] = visits
-
+   visitor_cookie_handler(request)
+   context_dict ['visits'] = request.session['visits'] 
+   
    response = render(request, 'rango/index.html', context_dict)
    
-   visitor_cookie_handler(request,response)
+   
 
    
    return response
